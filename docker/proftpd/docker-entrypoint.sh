5ad4134860e8c697287af57d463d3783b4128d9f
rym.the.great <rym.the.great@gmail.com>
2023-05-09 19:11:55 +0300

    Change users

--- docker/proftpd/docker-entrypoint.sh
+++ docker/proftpd/docker-entrypoint.sh
@@ -2,10 +2,9 @@
 
 while IFS=";" read -r ftp_username ftp_userpass ftp_useruid
 do
-	adduser --shell /bin/sh -D -u ${ftp_useruid} -G users -h /home/$ftp_username $ftp_username
-	addgroup $ftp_username ${PROFTPDGRP}
-	echo "${ftp_username}:${ftp_userpass}" | chpasswd
-  chown -R $ftp_username:${USER_GROUP} /home/$ftp_username
+  [ ! -d "/home/$ftp_username" ] && mkdir -p "/home/$ftp_username"
+  echo ${ftp_userpass} | /usr/local/proftpd/contrib/ftpasswd --stdin --passwd --file="${PROFTPD_PASSWD_FILE}" --name=${ftp_username} --uid=${USER_UID} --gid=${USER_GID} --home=/home/$ftp_username --shell=/sbin/nologin
+  chown -R ${USER_UID}:${USER_GID} /home/$ftp_username
 done < "/opt/userlist.csv"
 envsubst < /etc/proftpd/proftpd.conf.template > /etc/proftpd/proftpd.conf
 
